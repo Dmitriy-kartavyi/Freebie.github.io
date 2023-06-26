c8cc3bddf19b1a6735be8919c2279095bb72ac07
Dmitriy-kartavyi <135726939+Dmitriy-kartavyi@users.noreply.github.com>
2023-06-26 16:04:29 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('.slider').slick({
+      waitForAnimate:false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: true,
+      appendDots: '.header__dots',
+      }
+      
+   )
+});
+
+$(document).ready(function(){
+	$('.slider__people').slick({
+      waitForAnimate:false,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: false,
+      appendArrows: '.block__arrows',
+      }
+      
+   )
+});
+
